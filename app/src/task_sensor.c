acd86ac5d48f417f24088c6b526753c71a762856
Martin Klöckner <mjkloeckner@gmail.com>
2025-09-26 18:47:14 -0300

    [not working] define `PIN_*` button id

--- app/src/task_sensor.c
+++ app/src/task_sensor.c
@@ -61,7 +61,13 @@
 /********************** internal data declaration ****************************/
 const task_sensor_cfg_t task_sensor_cfg_list[] = {
 	{ID_BTN_A,  BTN_A_PORT,  BTN_A_PIN,  BTN_A_PRESSED, DEL_BTN_XX_MAX,
-	 EV_SYS_IDLE,  EV_SYS_LOOP_DET}
+	 EV_SYS_IDLE,  EV_SYS_LOOP_DET},
+	{ID_BTN_B,  BTN_A_PORT,  D2_PIN,  BTN_A_PRESSED, DEL_BTN_XX_MAX,
+	 EV_SYS_IDLE,  EV_SYS_LOOP_DET},
+	{ID_BTN_C,  BTN_A_PORT,  BTN_A_PIN,  BTN_A_PRESSED, DEL_BTN_XX_MAX,
+	 EV_SYS_IDLE,  EV_SYS_LOOP_DET},
+	{ID_BTN_D,  BTN_A_PORT,  BTN_A_PIN,  BTN_A_PRESSED, DEL_BTN_XX_MAX,
+     EV_SYS_IDLE,  EV_SYS_LOOP_DET}
 };
 
 #define SENSOR_CFG_QTY	(sizeof(task_sensor_cfg_list)/sizeof(task_sensor_cfg_t))
